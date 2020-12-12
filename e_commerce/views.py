26d3e31118b6f5798806b2a30cc70747cb73f990
Rodrigo Ortega Tierno <rodrigo.tierno14@gmail.com>
2020-12-12 19:55:57 -0300

    update e_commerce/forms.py, e_commerce/views.py, create and set template/auth/login.html and template/auth/register.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,22 +1,68 @@
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm, RegisterForm
+
 def home_page(request):
     context = {
-        "title": "Página principal",
-        "content": "Bem-vindo a página principal"
+        "title": "Home Page",
+        "content": "Bem-vindo a Home page"
     }
+    if request.user.is_authenticated:
+        context["premium_content"]= "Você é um usuário Premium"
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
-        "title": "Página sobre",
-        "content": "Bem-vindo a página sobre"
+        "title": "About page",
+        "content": "Bem-vindo a About Page"
     }
     return render(request, "about/view.html", context)
 
 def contact_page(request):
     context = {
-        "title": "Página contato",
-        "content": "Bem-vindo a página de contato"
+        "title": "Contact Page",
+        "content": "Bem-vindo a Contact Page"
     }
     return render(request, "contact/view.html", context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    #print(request.user.is_authenticaded)
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = athenticate(request, username=username, password=password)
+        print(user)
+        #print(request.user.is_authenticated)
+        if user is not None:
+            #print(request.user.is_authenticated)
+            login(request, user)
+            print("Login válido")
+            #Redireciona para uma página de sucesso.
+            return redirect("/")
+        else:
+            #Retorna uma mensagem de erro de 'invalid login'.
+            print("Login inválido")
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(reques, "auth/register.html", context)
