a8bc931a5d1e9d03ae6e17781622e3764081be33
Rodrigo Ortega Tierno <rodrigo.tierno14@gmail.com>
2020-12-15 13:14:53 -0300

    create template in src/products/templates/products/list.html, add function based view in src/products/views.py, import and add to urlpatterns the fbv

--- products/views.py
+++ products/views.py
@@ -2,8 +2,16 @@ from django.views.generic import ListView
 from django.shortcuts import render
 
 from .models import Product
-
+#class based view
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
-    
+    template_name = "products/list.html"
+
+#function based view
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
