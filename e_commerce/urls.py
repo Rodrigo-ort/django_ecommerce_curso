3da4e59a7ebba7ff7ae91f060f78f0c6034f835b
Rodrigo Ortega Tierno <rodrigo.tierno14@gmail.com>
2020-12-15 12:56:19 -0300

    crate cbv in /src/products/views.py, import it in /src/e_commerce/urls.py and add to urlpatterns

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path
+
+from products.views import ProductListView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -26,6 +28,7 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
     path('admin/', admin.site.urls),
 ]
 
