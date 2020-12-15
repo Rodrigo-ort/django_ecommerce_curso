25350e1441491ac49425e2f7fbd8a25ebfd14159
Rodrigo Ortega Tierno <rodrigo.tierno14@gmail.com>
2020-12-15 15:52:42 -0300

    import detail view and put it in urlpatterns in src/e_commerce/urls.py

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path
 
 #from products.views import ProductListView, product_list_view
-from products.views import ProductListView
+from products.views import ProductListView, ProductDetailView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -31,6 +31,7 @@ urlpatterns = [
     path('register/', register_page),
     path('products/', ProductListView.as_view()),
     #path('products-fbv/', product_list_view),
+    path('products/<int:pk>', ProductDetailView.as_view()),
     path('admin/', admin.site.urls),
 ]
 
