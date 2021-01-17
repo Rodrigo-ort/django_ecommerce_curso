5cbc84a37b1e6d42060d92d585962fa0d63f2dac
Rodrigo Ortega Tierno <rodrigo.tierno14@gmail.com>
2021-01-17 02:00:44 -0300

    create src/products/urls.py

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import (
+                        ProductListView,
+                        ProductDetailSlugView,)
+
+urlpatterns = [
+    path('', ProductListView.as_view()),
+    path('<slug:slug>', ProductDetailSlugView.as_view())
+]
