eade382409672322e39864d50b9b2949c3d0e903
Rodrigo Ortega Tierno <rodrigo.tierno14@gmail.com>
2021-02-24 15:05:40 -0300

    create search/urls.py

---
+++ search/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+app_name = "search"
+
+from products.views import (
+    ProductListView,
+)
+
+urlpatterns = [
+    path('', ProductListView.as_view(), name='list')
+]
